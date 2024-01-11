836080e8c4957fecd12bd42c96e6075f9ca6b133
Aaron Villalpando <aaron@boundaryml.com>
2024-01-11 14:39:15 -0800

    update classification guide

--- classification-guide/main.py
+++ classification-guide/main.py
@@ -1,17 +1,20 @@
 from baml_client import baml as b
-# Import your generated Email model. 
+
+# Import your generated Email model.
 # We generate this pydantic model for you.
-from baml_client.baml_types import Intent
+from baml_client.baml_types import Category
 import asyncio
+
 
 # You can call a specific version using
 # b.GetOrderInfoV1.get_impl("version1").run(Email(...). Otherwise it will use the default_impl in the function definition.
 async def main():
-  customer_intent = await b.ClassifyIntent("I want to cancel my order")
-  if customer_intent == Intent.CancelOrder:
-      print("Customer wants to cancel order")
-  else:
-      print("Customer wants to {}".format(customer_intent))
+    customer_intent = await b.ClassifyMessage("I want to cancel my order")
+    if customer_intent == Category.CancelOrder:
+        print("Customer wants to cancel order")
+    else:
+        print("Customer wants to {}".format(customer_intent))
+
 
 if __name__ == "__main__":
-  asyncio.run(main())
+    asyncio.run(main())
