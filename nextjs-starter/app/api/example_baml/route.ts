5ccbdac4f501bf901f8b760d2f02c19277f5dc7e
Vaibhav Gupta <vbv@gloo.chat>
2024-05-25 09:16:52 -0700

    Working runtime

--- nextjs-starter/app/api/example_baml/route.ts
+++ nextjs-starter/app/api/example_baml/route.ts
@@ -1,17 +1,31 @@
 export const dynamic = 'force-dynamic'
 
-import b from '../../../baml_client'
+import { b, traceAsync } from "../../../baml_client"
 import { Role } from '../../../baml_client/types';
 
 export async function POST(request: Request) {
-    const result = await b.ClassifyMessage({
-        convo: [
+    const result = await b.ClassifyMessage(
+        [
             {
                 role: Role.Customer,
                 content: "I want to cancel my subscription"
             }
         ]
-    });
+    );
+
+    // const stream = b.stream.ClassifyMessage([
+    //     {
+    //         role: Role.Customer,
+    //         content: "I want to cancel my subscription"
+    //     }
+    // ]);
+
+    // for await (const event of stream) {
+    //     console.log("STREAM:", event);
+    // }
+
+    // const result2 = await stream.done();
+    // console.log("FINAL:", result2);
 
     return Response.json(result);
 }
--- nextjs-starter/next.config.mjs
+++ nextjs-starter/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@boundaryml/baml-core"],
+    serverComponentsExternalPackages: ["@boundaryml/baml"],
   },
   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
     config.module.rules.push({
@@ -15,6 +15,21 @@ const nextConfig = {
         },
       ],
     });
+
+    // Adding new rule for .baml files
+    config.module.rules.push({
+      test: /\.baml$/, // Match files ending with .baml
+      use: [
+        {
+          loader: 'file-loader', // Using file-loader to manage .baml files
+          options: {
+            name: '[name].[ext]', // Naming format for the output files
+            outputPath: 'baml_src/', // Directory where .baml files will be placed
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
