d37c5d5fe6e4ac1ef331b3a7c9ec04009c8eb3b7
Aaron Villalpando <aaron@boundaryml.com>
2024-05-25 10:45:31 -0700

    try make it work

--- nextjs-starter/next.config.mjs
+++ nextjs-starter/next.config.mjs
@@ -1,3 +1,5 @@
+import CopyPlugin from "copy-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -17,18 +19,34 @@ const nextConfig = {
     });
 
     // Adding new rule for .baml files
-    config.module.rules.push({
-      test: /\.baml$/, // Match files ending with .baml
-      use: [
-        {
-          loader: 'file-loader', // Using file-loader to manage .baml files
-          options: {
-            name: '[name].[ext]', // Naming format for the output files
-            outputPath: 'baml_src/', // Directory where .baml files will be placed
-          },
-        },
-      ],
-    });
+    // config.module.rules.push({
+    //   test: /\.baml$/, // Match files ending with .baml
+    //   use: [
+    //     {
+    //       loader: 'file-loader', // Using file-loader to manage .baml files
+    //       options: {
+    //         name: '[name].[ext]', // Naming format for the output files
+    //         outputPath: 'baml_src/', // Directory where .baml files will be placed
+    //       },
+    //     },
+    //   ],
+    // });
+
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: "baml_src/", to: "baml_src/" },
+        ],
+      }),
+    )
+
+    // config.module.rules.push(
+    //   {
+    //     test: /\.baml$/i,
+    //     loader: 'raw-loader',
+     
+  
+    // });
 
     return config;
   },
