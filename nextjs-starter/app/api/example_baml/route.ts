63208cd77921b544c12006e663ca476de3dc2065
Aaron Villalpando <aaron@boundaryml.com>
2024-05-27 22:38:35 -0700

    runtime initial version in ts

--- nextjs-starter/app/api/example_baml/route.ts
+++ nextjs-starter/app/api/example_baml/route.ts
@@ -13,12 +13,9 @@ export async function POST(request: Request) {
         ]
     );
 
-    // const stream = b.stream.ClassifyMessage([
-    //     {
-    //         role: Role.Customer,
-    //         content: "I want to cancel my subscription"
-    //     }
-    // ]);
+    // const stream = b.stream.ExtractResume(
+    //     "Sarah Montez\nHarvard University\nMay 2015-2019\n3.92 GPA\nGoogle\nSoftware Engineer\nJune 2019-Present\n- Backend engineer\n- Rewrote search and uplifted metrics by 120%\n- Used C++ and Python\nMicrosoft\nSoftware Intern\nJune 2018-August 2018\n- Worked on the Windows team\n- Updated the UI\n- Used C++"
+    // );
 
     // for await (const event of stream) {
     //     console.log("STREAM:", event);
