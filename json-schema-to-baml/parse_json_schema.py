8c7d27b9e3682deead035df8167c6c577c6d2f21
hellovai <vbv@boundaryml.com>
2024-11-26 16:06:53 -0800

    Add JSON schema example

---
+++ json-schema-to-baml/parse_json_schema.py
@@ -0,0 +1,102 @@
+from typing import Any, Dict
+from baml_client.type_builder import TypeBuilder, FieldType
+
+from pydantic import BaseModel
+
+class SchemaAdder:
+    def __init__(self, tb: TypeBuilder, schema: Dict[str, Any]):
+        self.tb = tb
+        self.schema = schema
+        self._ref_cache = {}
+    
+
+
+    def _parse_object(self, json_schema: Dict[str, Any]) -> FieldType:
+        assert json_schema["type"] == "object"
+        name = json_schema.get("title")
+        if name is None:
+            raise ValueError("Title is required in JSON schema for object type")
+        
+        required_fields = json_schema.get("required", [])
+        assert isinstance(required_fields, list)
+
+        new_cls = self.tb.add_class(name)
+        if properties := json_schema.get("properties"):
+            assert isinstance(properties, dict)
+            for field_name, field_schema in properties.items():
+                assert isinstance(field_schema, dict)
+                default_value = field_schema.get("default")
+                field_type = self.parse(field_schema)
+                if field_name not in required_fields:
+                    if default_value is None:
+                        field_type = field_type.optional()
+                property = new_cls.add_property(field_name, field_type)
+                if description := field_schema.get("description"):
+                    assert isinstance(description, str)
+                    if default_value is not None:
+                        description = description.strip() + "\n" + f"Default: {default_value}"
+                        description = description.strip()
+                    if len(description) > 0:
+                        property.description(description)
+        return new_cls.type()
+
+    def _parse_string(self, json_schema: Dict[str, Any]) -> FieldType:
+        assert json_schema["type"] == "string"
+        title = json_schema.get("title")
+        
+        if enum := json_schema.get("enum"):
+            assert isinstance(enum, list)
+            if title is None:
+                # Treat as a union of literals
+                return self.tb.union([self.tb.literal_string(value) for value in enum])
+            new_enum = self.tb.add_enum(title)
+            for value in enum:
+                new_enum.add_value(value)
+            return new_enum.type()
+        return self.tb.string()
+
+    def _load_ref(self, ref: str) -> FieldType:
+        assert ref.startswith("#/"), f"Only local references are supported: {ref}"
+        _, left, right = ref.split("/", 2)
+        
+        if ref not in self._ref_cache:
+            if refs := self.schema.get(left):
+                assert isinstance(refs, dict)
+                if right not in refs:
+                    raise ValueError(f"Reference {ref} not found in schema")
+                self._ref_cache[ref] = self.parse(refs[right])
+        return self._ref_cache[ref]
+
+    def parse(self, json_schema: Dict[str, Any]) -> FieldType:
+        if any_of := json_schema.get("anyOf"):
+            assert isinstance(any_of, list)
+            return self.tb.union([self.parse(sub_schema) for sub_schema in any_of])
+        
+        if ref := json_schema.get("$ref"):
+            assert isinstance(ref, str)
+            return self._load_ref(ref)
+
+        type_ = json_schema.get("type")
+        if type_ is None:
+            raise ValueError(f"Type is required in JSON schema: {json_schema}")
+        parse_type = {
+            "string": lambda: self._parse_string(json_schema),
+            "number": lambda: self.tb.float(),
+            "integer": lambda: self.tb.int(),
+            "object": lambda: self._parse_object(json_schema),
+            "array": lambda: self.parse(json_schema).list(),
+            "boolean": lambda: self.tb.bool(),
+            "null": lambda: self.tb.null(),
+        }
+
+        if type_ not in parse_type:
+            raise ValueError(f"Unsupported type: {type_}")
+
+        field_type = parse_type[type_]()
+
+        return field_type
+
+
+def parse_json_schema(json_schema: Dict[str, Any], tb: TypeBuilder) -> FieldType:
+    parser = SchemaAdder(tb, json_schema)
+    return parser.parse(json_schema)
---
+++ json-schema-to-baml/test_one.py
@@ -0,0 +1,70 @@
+from dotenv import load_dotenv
+import os
+from baml_client import b, reset_baml_env_vars
+from parse_json_schema import parse_json_schema
+from baml_client.type_builder import TypeBuilder
+from enum import Enum
+from typing import Union
+
+from typing_extensions import Annotated
+
+from pydantic import BaseModel, Field
+from pydantic.config import ConfigDict
+
+
+# Load environment variables
+load_dotenv()
+# Reset BAML environment variables to the most recent values
+print(os.environ['OPENAI_API_KEY'])
+reset_baml_env_vars(os.environ.copy())
+
+class FooBar(BaseModel):
+    count: int
+    size: Union[float, None] = None
+
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+    other = 'other'
+    not_given = 'not_given'
+
+
+class MainModel(BaseModel):
+    """
+    This is the description of the main model
+    """
+
+    model_config = ConfigDict(title='Main')
+
+    foo_bar: FooBar
+    gender: Annotated[Union[Gender, None], Field(alias='Gender')] = None
+    snap: int = Field(
+        default=42,
+        title='The Snap',
+        description='this is the value of snap',
+        gt=30,
+        lt=50,
+    )
+
+
+
+def parse(raw_text: str):
+    tb = TypeBuilder()
+    res = parse_json_schema(MainModel.model_json_schema(), tb)
+    tb.DyanamicContainer.add_property("data", res)
+    response = b.ExtractDynamicTypes(raw_text, { "tb": tb })
+
+    # Sadly nothing in static analysis can help us here
+    # its a type defined at runtime!
+    data = response.data # type: ignore
+
+    # This is guaranteed to be succeed thanks to BAML!
+    content = MainModel.model_validate(data)
+
+
+def test_one():
+    parse("Make an example!")
+
+if __name__ == "__main__":
+    test_one()
